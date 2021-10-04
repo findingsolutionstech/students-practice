ae86801dd974d855de0d811ad848d10e4fa96ab9
ccanada1 <chriscanada2011@yahoo.com>
2021-10-04 19:56:59 -0400

    counting from 1-30

--- src/classassignments/firstclass.java
+++ src/classassignments/firstclass.java
@@ -1,5 +1,14 @@
 package classassignments;
 
 public class firstclass {
-
+	
+	public static void duck (String[] args) {
+		
+		for (int i = 1; i <= 30; i++ )
+		System.out.println(i);
+				
+		
+	}
 }
+
+
