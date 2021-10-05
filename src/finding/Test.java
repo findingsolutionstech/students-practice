2fad55ff8d6e931c0fd56b938d901311893d6d16
Sushan <findingsolutionstech@gmail.com>
2021-10-05 17:43:15 -0500

    cleanup

--- src/finding/Test.java
+++ src/finding/Test.java
@@ -1,26 +1,23 @@
 package finding;
 
 public class Test {
-	
-	/* Fizz Buzz - Write a program that prints numbers from 1 to 30. 
-	 * But for multiples of 3 print "Fizz" instead of number and "Buzz" for multiples of 5.
-	 * If there are multiples of both, print "FizzBuzz" 
+
+	/* Fizz Buzz - Write a program that prints numbers from 1 to 30. But for
+	 * multiples of 3 print "Fizz" instead of number and "Buzz" for multiples of 5.
+	 * If there are multiples of both, print "FizzBuzz"
 	 */
 	public static void main(String[] args) {
-		
-		for(int i = 1; i <= 30; i++) {
-			
-			int remainderForThree = i % 3;
-			int remainderForFive = i % 5;
-			
-			if(remainderForThree == 0) {  			// use / to get quotient, % to get remainder
+
+		for (int i = 1; i <= 30; i++) {
+
+			if (i % 3 == 0) { // use / to get quotient, % to get remainder
 				System.out.println("Fizz");
-			} else if (remainderForFive == 0){
+			} else if (i % 5 == 0) {
 				System.out.println("Buzz");
 			} else {
 				System.out.println(i);
 			}
 		}
-		
+
 	}
 }
