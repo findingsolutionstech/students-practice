71edce007656e46a558aeb7eddb89a0ddae58226
ccanada1 <chriscanada2011@yahoo.com>
2021-10-04 20:33:26 -0400

    counting in multiples of 3 using if

--- src/classassignments/firstclass.java
+++ src/classassignments/firstclass.java
@@ -1,13 +1,17 @@
 package classassignments;
-
+//counting from 1-30)
 public class firstclass {
 	
-	public static void duck (String[] args) {
+	public static void main (String[] args) {
 		
-		for (int i = 1; i <= 30; i++ )
-		System.out.println(i);
-				
-		
+		for (int i = 1; i <= 30; i++ ) {
+			if(i % 3 == 0) {
+				System.out.println("Fizz");
+				} else {
+					System.out.println(i);
+				}
+			}
+	
 	}
 }
 
