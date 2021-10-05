4d11130ac3ce0b805cadcd04e0ef262cbe504d21
Shirley <shirleyannwarrjones@gmail.com>
2021-10-04 20:03:04 -0400

    Help me

--- src/faith/Hope.java
+++ src/faith/Hope.java
@@ -2,4 +2,10 @@ package faith;
 
 public class Hope {
 
+public static void main(string[] args) {
+	
+	// use or loops to print 1 to 30 in console
+		for(int 1-1;) 1<= 30; i++) {
+			System.out.println(1);
+		}
 }
