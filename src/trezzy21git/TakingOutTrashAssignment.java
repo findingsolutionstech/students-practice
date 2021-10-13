f7fd79a55999303ec9dfbc268ec9fc2eb96407e0
trezzy21 <tscottnpl@gmail.com>
2021-10-12 20:19:50 -0400

    modifed taking out TrashAssignment

--- src/trezzy21git/TakingOutTrashAssignment.java
+++ src/trezzy21git/TakingOutTrashAssignment.java
@@ -5,24 +5,24 @@ public class TakingOutTrashAssignment {
 	
 	//taking out the trash
 	//make array and then loop through the array and print everything out
+	//print reverse
 	
 	public static void main(String[]args) {
 	
 		
 	String [] steps = new String[] {" 1. get up", "2. walk to the trash", "3. pick up trash",
 				"4. take trash outside", "5. dump the trash in the can"};
+	String[]steps2=new String[] {"a","b","c"};
 		
 		for(int i=0;i<steps.length;i++) {
 		System.out.println(steps[i]);
 		
 		
-	int b=0;
-	while(b<steps.length) {
-		for( b=0;b<steps.length;b++) {
-			System.out.println(steps[b]);
+		for(int b=2;b<=steps2.length;b--) {
+			System.out.println(steps2[b]);
 	}
 	}
 	}
 	}
-}
+
 	
