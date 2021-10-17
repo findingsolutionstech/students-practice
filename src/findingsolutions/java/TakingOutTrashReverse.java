a09bfaf71935bbb7aad70a6d2dcadaf7af6dd5e2
Sushan <findingsolutionstech@gmail.com>
2021-10-17 17:20:41 -0500

    updating loop classes

--- src/findingsolutions/java/TakingOutTrashReverse.java
+++ src/findingsolutions/java/TakingOutTrashReverse.java
@@ -1,22 +1,14 @@
 package findingsolutions.java;
 
-public class TakingOutTrash {
+public class TakingOutTrashReverse {
 
 	public static void main(String[] args) {
 
-		String[] steps = new String[] { "Step 1", "Step 2", "Step 3" }; // 0th, 1th, 2th item/element
+		String[] steps = { "Step 1", "Step 2", "Step 3" }; // 0th, 1th, 2th item/element
 
-		printingArray(steps);
-		System.out.println();
 		printingReverse(steps);
 		System.out.println();
 		printingReverseAlternate(steps);
-	}
-
-	static void printingArray(String[] steps) {
-		for (int i = 0; i < steps.length; i++) { // steps.length = 3
-			System.out.println(steps[i]);
-		}
 	}
 
 	static void printingReverse(String[] steps) {
