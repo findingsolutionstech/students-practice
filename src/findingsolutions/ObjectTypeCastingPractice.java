ca0099516842df162d30d75a40a7c23a44e34e8d
trezzy21 <tscottnpl@gmail.com>
2021-11-14 17:47:39 -0500

    adding in some files

--- src/findingsolutions/ObjectTypeCastingPractice.java
+++ src/findingsolutions/ObjectTypeCastingPractice.java
@@ -1,5 +1,22 @@
 package findingsolutions;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ObjectTypeCastingPractice {
 
+		public static void main(String[]args) {
+			//Casting from a subclass(cat) to a superclass(Animal) is called upcasting
+			//implicitly or automatically handled by java compilers
+			//Animal is super class and cat is subclass
+			Animal animal=new Cat("Bobby");
+			
+			
+			//Casting from a superclass (Animal) to subclass (cat) is called downcasting
+			//manual casting required
+			Cat cat=(Cat)new Animal();
+			
+			
+			List<Integer>nums=new ArrayList<>();
+		}
 }
--- src/findingsolutions/UnboxingAutoboxingPractice.java
+++ src/findingsolutions/UnboxingAutoboxingPractice.java
@@ -1,5 +1,43 @@
 package findingsolutions;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class UnboxingAutoboxingPractice {
+	
+	public static void main(String[]args) {
+		
+		/*autoboxing is automatic change
+		 * used to convert primitive data type (like int data type) to obejct wrapper type (Like Integer)
+		 * 
+		 */
+		
+		List<Integer>listOfNumbers=new ArrayList<>();
+		for(int i =1; i <50;i++) {
+			listOfNumbers.add(i); //equivalent to doing listOfNumbers.add(Integer.valueOf(i)):
+		}
+		//below wont work as collections expect object wrapper type not primitives
+		//while arrays can be of both primitives and object types List<int>
+		//List<int> listOfPrimitiveInt=newArrayList<>();
+		
+		int[]array= {};
+		Integer[]array2= {};
+		
+		/*conversion of object wrapper type (like Integer) to primitive type(like int)
+		 * 
+		 */
+		
+		Integer num = -8;
+		printNum(num);
+		
+		
+	}
+		public static void printNum(int num) {
+			System.out.println(num);
+		}
+		
+		
+		
+	
 
 }
