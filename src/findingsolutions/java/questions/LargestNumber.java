e3f05925ce9680e074193d6f7071f65b0bbe6ac5
findingsolutions <findingsolutionstech@gmail.com>
2021-12-06 19:11:52 -0600

    more java practice

---
+++ src/findingsolutions/java/questions/LargestNumber.java
@@ -0,0 +1,13 @@
+package findingsolutions.java.questions;
+
+public class LargestNumber {
+	
+	public static void main(String[] args) {
+		
+	}
+
+	/* Write a method that returns the largest number in a
+	 *  list of numbers 
+	 *  input - logic - output
+	 *  */
+}
